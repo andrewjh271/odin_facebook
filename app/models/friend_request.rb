6044584f4e98654d4587192e67ec6a85d1102931
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2020-12-24 00:07:03 -0800

    Added #no_request_to_current_friends validation

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -12,6 +12,7 @@ class FriendRequest < ApplicationRecord
   validates :recipient_id, uniqueness: { scope: :requester_id }
   validate :no_self_referential_friend_requests
   validate :request_in_one_direction_only
+  validate :no_requests_to_current_friends, if: -> { requester }
 
   belongs_to :requester, class_name: :User
   belongs_to :recipient, class_name: :User
@@ -20,13 +21,19 @@ class FriendRequest < ApplicationRecord
 
   def no_self_referential_friend_requests
     if requester_id == recipient_id
-      errors[:recipient_id] << 'No self referential friend requests allowed'
+      errors[:recipient_id] << 'No self referential friend requests allowed.'
     end
   end
 
   def request_in_one_direction_only
     if FriendRequest.exists?(requester_id: recipient_id, recipient_id: requester_id)
-      errors[:recipient_id] << 'There is a pending request from this user'
+      errors[:recipient_id] << 'There is a pending request from this user.'
+    end
+  end
+
+  def no_requests_to_current_friends
+    if requester.friends.any? { |friend| friend.id == recipient.id }
+      errors[:recipient_id] << 'User is already friends with this recipient.'
     end
   end
 end
