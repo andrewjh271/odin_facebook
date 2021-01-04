0e5e2d38eecf531e93226803731917a9b58a4c6a
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2021-01-03 21:54:38 -0800

    Permit :name for devise controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: :about
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def about
     render 'shared/about'
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
