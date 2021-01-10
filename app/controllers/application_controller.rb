7e5c43c99606e0354903da5f5dddd0dd26cb39b5
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2021-01-09 16:08:17 -0800

    Add search function to header; minor styling adjustments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: :about
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def about
-    render 'shared/about'
-  end
-
   protected
 
   def configure_permitted_parameters
