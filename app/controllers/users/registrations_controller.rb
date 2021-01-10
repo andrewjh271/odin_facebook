b141dd744b3ffd339eccdecada593354cdf3ac75
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2021-01-09 22:14:52 -0800

    Only execute after_action callbacks if @user successfully saves

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-  after_action :ensure_avatar, :create_friend_invitations, :send_welcome_email, only: :create
+  after_action :ensure_avatar, :create_friend_invitations, :send_welcome_email, only: :create, if: -> { @user.persisted? }
 
   # GET /resource/sign_up
   # def new
