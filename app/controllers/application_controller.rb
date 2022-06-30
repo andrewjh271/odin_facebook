e0bf98753c2871aa7aa5cd00ce80a93b43af0480
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2022-06-29 17:39:34 -0700

    Fix devise authorization bug involving custom error pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: :about
+  before_action :authenticate_user!, except: [:about, :not_found, :internal_server, :unprocessable]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
