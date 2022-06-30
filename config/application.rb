7b1caac8edadd5286a7aa94e2b27a12d38dadadc
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2022-06-30 00:09:13 -0700

    Update config.load_defaults to 6.1

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module OdinFacebook
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.1
 
     config.exceptions_app = self.routes
 
